ec349cb210d3255b715556697e70abebbf3524f2
June <kimjune01@gmail.com>
2018-03-04 13:42:21 -0800

    a new node websocket server

---
+++ server.js
@@ -0,0 +1,10 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  ws.send(Date.now());
+});
