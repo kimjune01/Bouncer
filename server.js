b0070d4ee46861a58e7a1ea184ecadb78f6de884
June <kimjune01@gmail.com>
2018-03-04 15:19:52 -0800

    round trip after POST response, add TODOs

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const WebSocket = require('ws');
 const Client = require('./client.js');
+var request = require('request');
 
 let clients = [];
 
-function clientFrom(ws) {
+function clientFromWebSocket(ws) {
   return clients.find(function(client) {
     return client.websocket == ws;
+  });
+};
+
+function clientFromID(id) {
+  return clients.find(function(client) {
+    return client.id == id;
   });
 };
 
@@ -29,16 +36,28 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
-    var client = clientFrom(ws);
+    var client = clientFromWebSocket(ws);
     if (client == null) {
       console.log("client not found!");
       // should not happen.
       return;
     }
     console.log("client.id: ", client.id);
-    var toSend = JSON.stringify({sender: client.id, payload: JSON.parse(message)});
-    console.log('toSend: ', toSend);
-    //TODO: synchronous POST this to an HTTPS endpoint
+    var toPost = JSON.stringify({sender: client.id, payload: JSON.parse(message)});
+    request.post({url:'https://httpbin.org/post',form: toPost}, function (error, response, body) {
+      if (error != null) {
+        console.log('error:', error); // Print the HTML for the Google homepage.
+        return;
+      }
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      //TODO: find destination from body
+      //var recipientID = body["recipientID"]
+      //var recipient = clientFromID(recipientID)
+      //TODO: get payload from body
+      //var payload = body["payload"]
+      //recipient.websocket.send(payload)
+      ws.send(body)
+    });
     // then, parse the response and send its payload to its recipient
   });
 
